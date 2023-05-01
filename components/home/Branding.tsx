12c91a29b33936b429ccfd8d7c00c6d08fd87ad2
Patryk Nawolski <patryknawolski@Patryks-MacBook-Air.local>
2023-05-01 09:58:57 +0200

    Adjust 'Customise your firm's branding' section

--- components/home/Branding.tsx
+++ components/home/Branding.tsx
@@ -2,9 +2,8 @@ import type { NextPage } from "next";
 import { useRef, useState } from "react";
 import Slider from "react-slick";
 import Image from "next/image";
-import { Box, Heading, Text, Grid, GridItem, Button } from "@chakra-ui/react";
-
-import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
+import { Box, Heading, Text, Grid, GridItem } from "@chakra-ui/react";
+import { Hide } from "@chakra-ui/react";
 
 import BrandingImg from "public/images/new-branding1.png";
 import Branding1Img from "public/images/branding1.svg";
@@ -17,8 +16,8 @@ const Branding: NextPage = () => {
     dots: false,
     arrows: false,
     infinite: false,
-    autoplay: false,
-    speed: 0,
+    autoplay: true,
+    speed: 300,
     slidesToShow: 1,
     slidesToScroll: 1,
     vertical: true,
@@ -83,7 +82,7 @@ const Branding: NextPage = () => {
     >
       <Box
         mb={{ base: "40px", md: "60px" }}
-        padding={{ base: "0 20px", md: "0" }}
+        padding={{ base: "0 25px", md: "0" }}
       >
         <Heading
           as="h3"
@@ -106,7 +105,7 @@ const Branding: NextPage = () => {
         </Heading>
 
         <Text
-          maxW="842px"
+          maxW="742px"
           margin="0 auto"
           color="#6C6B74"
           textAlign={{ base: "left", md: "center" }}
@@ -117,124 +116,125 @@ const Branding: NextPage = () => {
         </Text>
       </Box>
 
-      <Box w="100%" position="relative">
-        <Grid
-          gap="40px"
-          templateColumns={{ base: "repeat(1, 1fr)", md: "repeat(5, 1fr)" }}
-        >
-          <GridItem colSpan={{ base: 5, md: 3 }}>
-            <Slider {...settings} ref={sliderRef} afterChange={handleChange}>
-              <Box
-                background={{
-                  base: 'url("/images/circle.webp") no-repeat center center',
-                  md: `url("/images/bg-branding1.png") no-repeat left center`,
-                }}
-                backgroundSize={{
-                  base: "cover !important",
-                  md: "contain !important",
-                }}
-                padding={{ base: "35px 25px", md: "102px 0" }}
-                display="flex"
-                alignItems="center"
-                justifyContent={{ base: "center", md: "flex-start" }}
-                w="100%"
-              >
-                <Box
-                  ml={{ base: "0", md: "106px" }}
-                  maxW={{ base: "332px", md: "653px", xxl: "100%" }}
-                >
-                  <Image alt="" src={BrandingImg} priority />
-                </Box>
-              </Box>
-              <Box
-                background={{
-                  base: 'url("/images/circle.webp") no-repeat center center',
-                  md: `url("/images/bg-branding1.webp") no-repeat left center`,
-                }}
-                backgroundSize={{
-                  base: "cover !important",
-                  md: "contain !important",
-                }}
-                padding={{ base: "35px 20px", md: "102px 0" }}
-                display="flex"
-                alignItems="center"
-                justifyContent={{ base: "center", md: "flex-start" }}
-                w="100%"
-              >
-                <Box
-                  ml={{ base: "0", md: "106px" }}
-                  maxW={{ base: "332px", md: "653px", xxl: "100%" }}
-                >
-                  <Image alt="" src={Branding1Img} priority />
-                </Box>
-              </Box>
-              <Box
-                background={{
-                  base: 'url("/images/circle.webp") no-repeat center center',
-                  md: `url("/images/bg-branding3.png") no-repeat left center`,
-                }}
-                backgroundSize={{
-                  base: "cover !important",
-                  md: "contain !important",
-                }}
-                padding={{ base: "35px 25px", md: "102px 0" }}
-                display="flex"
-                alignItems="center"
-                justifyContent={{ base: "center", md: "flex-start" }}
-                w="100%"
-              >
-                <Box
-                  ml={{ base: "0", md: "106px" }}
-                  maxW={{ base: "332px", md: "653px", xxl: "100%" }}
-                >
-                  <Image alt="" src={Branding2Img} priority />
-                </Box>
-              </Box>
-            </Slider>
-          </GridItem>
-
-          <GridItem colSpan={{ base: 5, md: 2 }}>
+      <Grid
+        gap={{ base: "10px", sm: "40px" }}
+        templateColumns={{ base: "repeat(1, 1fr)", md: "repeat(5, 1fr)" }}
+      >
+        <GridItem colSpan={{ base: 5, md: 3 }}>
+          <Slider {...settings} ref={sliderRef} afterChange={handleChange}>
             <Box
-              w="100%"
-              h="100%"
-              alignItems="center"
+              background={{
+                base: 'url("/images/circle.webp") no-repeat center center',
+                md: `url("/images/bg-branding.webp") no-repeat left center`,
+              }}
+              backgroundSize={{
+                base: "cover !important",
+                md: "contain !important",
+              }}
+              padding={{ base: "35px 25px", md: "102px 0" }}
               display="flex"
-              justifyContent={{ base: "flex-end" }}
-              padding={{ base: "0 20px", md: 0 }}
+              alignItems="center"
+              justifyContent={{ base: "center", md: "flex-start" }}
+              w="100%"
             >
               <Box
-                maxW={{ base: "467px", xxl: "80%" }}
-                mr={{ base: "0", md: "40px", xl: "80px" }}
+                ml={{ base: "0", md: "106px" }}
+                maxW={{ base: "332px", md: "653px" }}
               >
-                <Box
-                  padding={{ base: "20px 25px", md: "25px 40px" }}
-                  borderRadius="10px"
-                  border={active === 0 ? "1px solid #E9E9F3" : ""}
-                  boxShadow={{
-                    base:
-                      active === 0
-                        ? "10px 10px 5px 0px rgba(53,53,61,0.05)"
-                        : "",
-                    md:
-                      active === 0
-                        ? "10px 10px 5px 0px rgba(53,53,61,0.05)"
-                        : "",
-                  }}
-                  _hover={{
-                    cursor: "pointer",
-                  }}
-                  onClick={() => handleActive(0)}
+                <Image alt="" src={BrandingImg} priority />
+              </Box>
+            </Box>
+            <Box
+              background={{
+                base: 'url("/images/circle.webp") no-repeat center center',
+                md: `url("/images/bg-branding1.webp") no-repeat left center`,
+              }}
+              backgroundSize={{
+                base: "cover !important",
+                md: "contain !important",
+              }}
+              padding={{ base: "35px 25px", md: "102px 0" }}
+              display="flex"
+              alignItems="center"
+              justifyContent={{ base: "center", md: "flex-start" }}
+              w="100%"
+            >
+              <Box
+                ml={{ base: "0", md: "106px" }}
+                maxW={{ base: "332px", md: "653px" }}
+              >
+                <Image alt="" src={Branding1Img} priority />
+              </Box>
+            </Box>
+            <Box
+              background={{
+                base: 'url("/images/circle.webp") no-repeat center center',
+                md: `url("/images/bg-branding2.webp") no-repeat left center`,
+              }}
+              backgroundSize={{
+                base: "cover !important",
+                md: "contain !important",
+              }}
+              padding={{ base: "35px 25px", md: "102px 0" }}
+              display="flex"
+              alignItems="center"
+              justifyContent={{ base: "center", md: "flex-start" }}
+              w="100%"
+            >
+              <Box
+                ml={{ base: "0", md: "106px" }}
+                maxW={{ base: "332px", md: "653px" }}
+              >
+                <Image alt="" src={Branding2Img} priority />
+              </Box>
+            </Box>
+          </Slider>
+        </GridItem>
+
+        <GridItem colSpan={{ base: 5, md: 2 }}>
+          <Box
+            w="100%"
+            h="100%"
+            alignItems="center"
+            display="flex"
+            justifyContent={{ sm: "flex-end" }}
+            padding={{ base: "0 25px", md: 0 }}
+          >
+            <Box
+              maxW="467px"
+              display={{ base: "flex", sm: "block" }}
+              alignItems={{ base: "center", sm: undefined }}
+              mr={{ base: "0", md: "40px", xl: "80px" }}
+            >
+              <Box
+                flex={{ base: "1 0 33.33%", sm: undefined }}
+                padding={{ base: "10px", sm: "25px 40px" }}
+                borderRadius="10px"
+                border={
+                  active === 0 ? "1px solid #E9E9F3" : "1px solid transparent"
+                }
+                boxShadow={{
+                  base:
+                    active === 0 ? "10px 10px 5px 0px rgba(53,53,61,0.05)" : "",
+                  md:
+                    active === 0 ? "10px 10px 5px 0px rgba(53,53,61,0.05)" : "",
+                }}
+                _hover={{
+                  cursor: "pointer",
+                }}
+                onClick={() => handleActive(0)}
+              >
+                <Heading
+                  as="h3"
+                  mb={{ sm: "12px" }}
+                  fontWeight="600"
+                  fontSize={{ base: "16px", md: "33px" }}
+                  lineHeight={{ base: "25px", md: "38px" }}
+                  color={active === 0 ? "#4959E7" : "#110F24"}
                 >
-                  <Heading
-                    as="h3"
-                    mb={{ base: "10px", md: "12px" }}
-                    fontWeight="600"
-                    fontSize={{ base: "20px", md: "33px" }}
-                    lineHeight={{ base: "25px", md: "38px" }}
-                    color={active === 0 ? "#4959E7" : "#110F24"}
-                  >
-                    Dye & Durham
-                  </Heading>
+                  Dye & Durham
+                </Heading>
+                <Hide below="sm">
                   <Text
                     fontWeight="normal"
                     fontSize={{ base: "14px", md: "18px" }}
@@ -245,37 +245,38 @@ const Branding: NextPage = () => {
                     Provides regulatory information, workflow technology,
                     property settlement services and legal support solutions.
                   </Text>
-                </Box>
+                </Hide>
+              </Box>
 
-                <Box
-                  padding={{ base: "20px 25px", md: "25px 40px" }}
-                  borderRadius="10px"
-                  border={active === 1 ? "1px solid #E9E9F3" : ""}
-                  boxShadow={{
-                    base:
-                      active === 1
-                        ? "10px 10px 5px 0px rgba(53,53,61,0.05)"
-                        : "",
-                    md:
-                      active === 1
-                        ? "10px 10px 5px 0px rgba(53,53,61,0.05)"
-                        : "",
-                  }}
-                  _hover={{
-                    cursor: "pointer",
-                  }}
-                  onClick={() => handleActive(1)}
+              <Box
+                flex={{ base: "1 0 33.33%", sm: undefined }}
+                padding={{ base: "10px", sm: "25px 40px" }}
+                borderRadius="10px"
+                border={
+                  active === 1 ? "1px solid #E9E9F3" : "1px solid transparent"
+                }
+                boxShadow={{
+                  base:
+                    active === 1 ? "10px 10px 5px 0px rgba(53,53,61,0.05)" : "",
+                  md:
+                    active === 1 ? "10px 10px 5px 0px rgba(53,53,61,0.05)" : "",
+                }}
+                _hover={{
+                  cursor: "pointer",
+                }}
+                onClick={() => handleActive(1)}
+              >
+                <Heading
+                  as="h3"
+                  mb={{ sm: "12px" }}
+                  fontWeight="600"
+                  fontSize={{ base: "16px", md: "33px" }}
+                  lineHeight={{ base: "25px", md: "38px" }}
+                  color={active === 1 ? "#4959E7" : "#110F24"}
                 >
-                  <Heading
-                    as="h3"
-                    mb={{ base: "10px", md: "12px" }}
-                    fontWeight="600"
-                    fontSize={{ base: "20px", md: "33px" }}
-                    lineHeight={{ base: "25px", md: "38px" }}
-                    color={active === 1 ? "#4959E7" : "#110F24"}
-                  >
-                    Kubota Australia Finance
-                  </Heading>
+                  Kubota Australia Finance
+                </Heading>
+                <Hide below="sm">
                   <Text
                     fontWeight="normal"
                     fontSize={{ base: "14px", md: "18px" }}
@@ -285,37 +286,38 @@ const Branding: NextPage = () => {
                     Offers secured equipment finance to businesses looking to
                     purchase new, demo and used Kubota equipment.
                   </Text>
-                </Box>
+                </Hide>
+              </Box>
 
-                <Box
-                  padding={{ base: "20px 25px", md: "25px 40px" }}
-                  borderRadius="10px"
-                  border={active === 2 ? "1px solid #E9E9F3" : ""}
-                  boxShadow={{
-                    base:
-                      active === 2
-                        ? "10px 10px 5px 0px rgba(53,53,61,0.05)"
-                        : "",
-                    md:
-                      active === 2
-                        ? "10px 10px 5px 0px rgba(53,53,61,0.05)"
-                        : "",
-                  }}
-                  _hover={{
-                    cursor: "pointer",
-                  }}
-                  onClick={() => handleActive(2)}
+              <Box
+                flex={{ base: "1 0 33.33%", sm: undefined }}
+                padding={{ base: "10px", sm: "25px 40px" }}
+                borderRadius="10px"
+                border={
+                  active === 2 ? "1px solid #E9E9F3" : "1px solid transparent"
+                }
+                boxShadow={{
+                  base:
+                    active === 2 ? "10px 10px 5px 0px rgba(53,53,61,0.05)" : "",
+                  md:
+                    active === 2 ? "10px 10px 5px 0px rgba(53,53,61,0.05)" : "",
+                }}
+                _hover={{
+                  cursor: "pointer",
+                }}
+                onClick={() => handleActive(2)}
+              >
+                <Heading
+                  as="h3"
+                  mb={{ sm: "12px" }}
+                  fontWeight="600"
+                  fontSize={{ base: "16px", md: "33px" }}
+                  lineHeight={{ base: "25px", md: "38px" }}
+                  color={active === 2 ? "#4959E7" : "#110F24"}
                 >
-                  <Heading
-                    as="h3"
-                    mb={{ base: "10px", md: "12px" }}
-                    fontWeight="600"
-                    fontSize={{ base: "20px", md: "33px" }}
-                    lineHeight={{ base: "25px", md: "38px" }}
-                    color={active === 2 ? "#4959E7" : "#110F24"}
-                  >
-                    SquareOne
-                  </Heading>
+                  SquareOne
+                </Heading>
+                <Hide below="sm">
                   <Text
                     fontWeight="normal"
                     fontSize={{ base: "14px", md: "18px" }}
@@ -326,12 +328,12 @@ const Branding: NextPage = () => {
                     the tools parents need to help their kids to be great with
                     money.
                   </Text>
-                </Box>
+                </Hide>
               </Box>
             </Box>
-          </GridItem>
-        </Grid>
-      </Box>
+          </Box>
+        </GridItem>
+      </Grid>
     </Box>
   );
 };
